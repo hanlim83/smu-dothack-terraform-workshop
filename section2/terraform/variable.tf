ec7849e9b6e865dda5586f460ba467811ff4d8f8
Lyney <30753183+hanlim83@users.noreply.github.com>
2024-10-18 20:18:35 +0800

    commit3

--- section2/terraform/variable.tf
+++ section2/terraform/variable.tf
@@ -1,16 +1,17 @@
 variable "tf_workshop_ex1_vpc_sg_id" {
+  type        = string
   description = "The ID of the VPC for Exercise 2"
-  default     = ["sg-0f9b9c5a891d88da4"]
+  default     = "sg-0f9b9c5a891d88da4"
 }
 
 variable "tf_workshop_ex1_subnet_id" {
+  type        = string
   description = "The ID of the Subnet for Exercise 2"
-  default     = ["subnet-0b9cc866875838ab6"]
+  default     = "subnet-0b9cc866875838ab6"
 }
 
-# This value is taken from the pipeline environment variables
 variable "student_id" {
   type        = string
   description = "Student ID"
-  default     = "student-9"
+  default     = "student-13"
 }
