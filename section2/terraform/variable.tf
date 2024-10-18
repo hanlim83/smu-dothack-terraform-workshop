49ead561a79615bc324b5283123c342c24723e55
Lyney <30753183+hanlim83@users.noreply.github.com>
2024-10-18 20:16:16 +0800

    commit

--- section2/terraform/variable.tf
+++ section2/terraform/variable.tf
@@ -1,10 +1,10 @@
 variable "tf_workshop_ex1_vpc_sg_id" {
-  description = "The ID of the VPC for Exercise 1"
+  description = "The ID of the VPC for Exercise 2"
   default     = ["sg-0f9b9c5a891d88da4"]
 }
 
 variable "tf_workshop_ex1_subnet_id" {
-  description = "The ID of the Subnet for Exercise 1"
+  description = "The ID of the Subnet for Exercise 2"
   default     = ["subnet-0b9cc866875838ab6"]
 }
 
