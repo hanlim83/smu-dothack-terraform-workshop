84d4737d0e5f5a39716f699e65198b0877549b03
Lyney <30753183+hanlim83@users.noreply.github.com>
2024-10-18 20:03:55 +0800

    Commit

--- section1/answer1/answer1A/main.tf
+++ section1/answer1/answer1A/main.tf
@@ -1,8 +1,8 @@
 # format: "resource_type" "resource_name"
 resource "aws_instance" "app_server" {
   # specify arguments here
-  ami                    = "ami-0ad522a4a529e7aa8" # EC2 AMI
-  instance_type          = "t3.micro"              # EC2 Instance Type
+  ami                    = "ami-0ad522a4a529e7aa8"
+  instance_type          = "t3.micro"
   subnet_id              = var.tf_workshop_ex1_subnet_id
   vpc_security_group_ids = [var.tf_workshop_ex1_vpc_sg_id]
 
